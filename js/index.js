422dfd7472bb075f1d7b1a519e619003d517a2bc
louisWin <lwin596@west-mec.org>
2022-02-22 17:58:19 -0700

    idk

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+      $(".box1").hide(1000);
+})
+
+
+if ("#media")
+$("#Media").mouseover(function() { 
+      $(".box1").show(500);
+})
+
+$("#mangaToggle").click(function() { 
+      $(".box2 > a").slideToggle(500);
+    
+})
