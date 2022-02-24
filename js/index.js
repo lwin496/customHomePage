945d9cd81297f0a53ff7c74ab8174cfdec03a5a4
louisWin <lwin596@west-mec.org>
2022-02-24 01:51:19 -0700

    revamed entire website

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
-$(document).ready(function(){ 
-      $(".box1").hide(1000);
-})
 
-
-if ("#media")
-$("#Media").mouseover(function() { 
-      $(".box1").show(500);
-})
-
-$("#mangaToggle").click(function() { 
-      $(".box2 > a").slideToggle(500);
+$(document).ready(function () {
+ 
+ 
+  $(".container2").hide()
+  $("#switchArrow").on("click",function() { 
+        $(".container1").slideToggle(1000);
+        $(".container2").toggle()
+        $(this).attr("src","images/arrowup.png")
+        $(this).addClass('test');
+  })
+  $(".test").on("click",function() { 
+      $(this).attr("src","images/arrowup.png")
+  })
+  function kroniiLeft() {   
+    $("#kronii").animate({left:"-=1000"} ,2000 ,"swing",kroniiRight),
+    $("#kronii").css({ 
+      transform:'rotateY(0deg)',
+    })
     
-})
+  
+  }
+  function kroniiRight() { 
+    $("#kronii").animate({left:"+=1000"} ,2000 ,"swing",kroniiLeft)
+    $("#kronii").css({ 
+      transform:'rotateY(-180deg)',
+    })
+  }
+  kroniiLeft()
+  
+
+});
